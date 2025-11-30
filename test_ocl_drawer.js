eba6a80e1c80a6ff93f809792f34d0629e1b2f68
kangmg <kangmg@korea.ac.kr>
2025-11-30 22:20:57 +0900

    remove temporary test files and ignore them

--- test_ocl_drawer.js
+++
@@ -1,9 +0,0 @@
-import OCL from 'openchemlib';
-
-console.log('Inspecting OCL Keys...');
-console.log(Object.keys(OCL));
-
-if (OCL.StructureView) console.log('StructureView found');
-if (OCL.CanvasDrawer) console.log('CanvasDrawer found');
-
-console.log('Done.');
